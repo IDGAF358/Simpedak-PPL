18a31904d7cbeb1fce65fde0ed2f201278299f0c
ChristYoga123 <christianuswibisono@gmail.com>
2023-04-20 13:53:32 +0700

    feat: integration client with spatie-laravel-permission

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Traits\HasRoles;
 
 class Client extends Model
 {
-    use HasFactory;
+    use HasFactory, HasRoles;
 
     protected $guarded = ["id"];
 
