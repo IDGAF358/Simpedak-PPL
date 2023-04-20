a30a509aae26a57295b88455fceb3c26376037d2
ChristYoga123 <christianuswibisono@gmail.com>
2023-04-20 13:35:22 +0700

    feat: create client data

---
+++ app/Models/Client.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ["id"];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+    ];
+}
